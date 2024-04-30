383e89ceba6cf0f98fe92eb50d8473d8c120c1f9
hasnain haider alvi <160486646+Hasnainalvi@users.noreply.github.com>
2024-04-30 17:38:09 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: "WHAT DO YOU LIKE TO ADD IN YOUR TODOS ?",
+    },
+
+    {
+      name: "addMore",
+      type: "confirm",
+      message: "DO YOU WANT TO ADD MORE IN IT ?",
+      default: "false",
+    },
+  ]);
+
+  todos.push(addTask.todo);
+  condition = addTask.addMore;
+  console.log(todos);
+};
